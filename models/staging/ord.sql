8bbf819f15aa5dfc544b3f5b23f3ab709e36e5b5
sudheer reddy <ksudheer.reddy366@gmail.com>
2025-12-05 02:21:25 +0000

    commit

--- models/staging/ord.sql
+++
@@ -1,8 +0,0 @@
-with orders_cte 
-(select order_id,
-        customer_id,
-        order_status,
-        total_amount 
-        from {{source('datafeed_shared_schema','orders')}})
-
-select * from orders_cte        
